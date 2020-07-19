705cb96bc255a80725aef6ade5107d03511aa2ad
Cristian Silva <cristian123105@gmail.com>
2020-07-19 14:42:39 -0300

    chore: update prject to use debug

--- api/src/database/migrations/20200422231904-delivery-man.js
+++ api/src/database/migrations/20200422231904-delivery-man.js
@@ -2,37 +2,36 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable("deliveryman",{
+    return queryInterface.createTable("deliverymans", {
       id: {
-				type: Sequelize.INTEGER,
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-			},
-			name: {
-        type:Sequelize.STRING,
-        allowNull: false
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
       },
-      avatar_id:{
-        type: Sequelize.STRING,
-        allowNull: true
-      },
-      email:{
+      name: {
         type: Sequelize.STRING,
         allowNull: false
       },
-			created_at: {
-				type: Sequelize.DATE,
-				allowNull: false,
-			},
-			update_at: {
-				type: Sequelize.DATE,
-				allowNull: false,
-			},
-    }),
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    },
+      {
+        freezeTableName: true,
+      })
   },
 
   down: (queryInterface, Sequelize) => {
-		return queryInterface.dropTable('deliveryman');
+    return queryInterface.dropTable('deliverymans');
   }
 };
