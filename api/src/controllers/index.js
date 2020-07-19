1aa2774054a3b209a15767f86e967f074223dd56
Cristian Silva <cristian123105@gmail.com>
2020-07-19 15:51:17 -0300

    chore: export all controllers from a index file

---
+++ api/src/controllers/index.js
@@ -0,0 +1,8 @@
+import DeliverymanController from './DeliverymanController';
+import FileController from './FileController';
+import OrderController from './OrderController';
+import RecipientsController from './RecipientsController';
+import SessionController from './SessionController';
+
+
+export { DeliverymanController, FileController, OrderController, RecipientsController, SessionController };
