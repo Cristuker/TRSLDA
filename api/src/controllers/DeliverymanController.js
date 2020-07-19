c060c771e573b15d7addedf25b0d850a4df72690
Cristian Silva <cristian123105@gmail.com>
2020-07-19 14:43:03 -0300

    feat: ensure return 500 when have internal error

--- api/src/controllers/DeliverymanController.js
+++ api/src/controllers/DeliverymanController.js
@@ -4,27 +4,32 @@ import * as Yup from 'yup';
 
 class DeliverymanController {
 
-    async store(req, res){
+    async store(req, res) {
 
-        const schema = Yup.object({
-            name: Yup.string().required(),
-            email: Yup.string().required(),
-            avatar_id: Yup.number()
-        })
+        try {
+            const schema = Yup.object({
+                name: Yup.string().required(),
+                email: Yup.string().required(),
+                avatar_id: Yup.number()
+            })
 
-        if(!(await schema.isValid(req.body))){
-            return res.status(401).json({ error: "Request body is not valid!" });
+            if (!(await schema.isValid(req.body))) {
+                return res.status(401).json({ error: "Request body is not valid!" });
+            }
+
+            const { name, email, avatar_id } = req.body;
+
+            const response = await Deliveryman.create({
+                name,
+                avatar_id,
+                email
+            });
+
+            return res.json({ response })
+        } catch (error) {
+            return res.status(500).json({ error: 'Server internal error' })
         }
 
-        const { name, email, avatar_id } = req.body;
-
-        const response = await Deliveryman.create({
-            name,
-            avatar_id,
-            email
-        });
-
-        return res.json({ response })
 
     }
 }
