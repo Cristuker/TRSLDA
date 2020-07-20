7c823871f498a512b4a729a9b83d074b8bff43f1
Cristian Silva <cristian123105@gmail.com>
2020-07-20 13:55:08 -0300

    refactor: put userId on request when i test token

--- api/src/middlewares/auth.js
+++ api/src/middlewares/auth.js
@@ -2,19 +2,19 @@ import jwt from 'jsonwebtoken';
 import { promisify } from 'util';
 import authConfig from '../config/auth';
 
-const authMiddleware = async (req, res, next) =>{
+const authMiddleware = async (req, res, next) => {
     const token = req.headers.authorization;
 
-    if(!token){
-        return res.status(401).json({ error: 'You need a authorization token'});
+    if (!token) {
+        return res.status(401).json({ error: 'You need a authorization token' });
     }
 
-    const [,jwtToken] = token.split(' ');
+    const [, jwtToken] = token.split(' ');
 
     try {
         const decoded = await promisify(jwt.verify)(jwtToken, authConfig.secret);
 
-        req.userId = decoded.id;
+        req.userId = decoded.user.id;
 
         return next();
     } catch (error) {
