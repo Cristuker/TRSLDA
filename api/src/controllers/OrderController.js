9fa813f97ef1721e83e498dce73dfb49afe75f9c
Cristian Silva <cristian123105@gmail.com>
2020-07-20 10:40:47 -0300

    feat: add store to order controller

--- api/src/controllers/OrderController.js
+++ api/src/controllers/OrderController.js
@@ -6,34 +6,32 @@ class OrderController {
 
     async store(req, res) {
 
-        try {
-            const schema = Yup.object({
-                recipient_id: Yup.string().required(),
-                deliveryman_id: Yup.string().required(),
-                signature_id: Yup.number(),
-                product: Yup.string().required(),
-                canceled_at: Yup.date(),
-                start_date: Yup.date(),
-                end_date: Yup.date(),
-            })
 
-            if (!(await schema.isValid(req.body))) {
-                return res.status(401).json({ error: "Request body is not valid!" });
-            }
+        const schema = Yup.object({
+            recipient_id: Yup.string().required(),
+            deliveryman_id: Yup.string().required(),
+            product: Yup.string().required().min(3),
+            // canceled_at: Yup.date(), put
+            start_date: Yup.date(),
+            // end_date: Yup.date(),
+        })
 
-            // const { name, email, avatar_id } = req.body;
-
-            // const response = await Order.create({
-            //     name,
-            //     avatar_id,
-            //     email
-            // });
-
-            return res.json({ response })
-        } catch (error) {
-            return res.status(500).json({ error: 'Server internal error' })
+        if (!(await schema.isValid(req.body))) {
+            return res.status(401).json({ error: "Request body is not valid!" });
         }
 
+        //A data de início deve ser cadastrada assim que for feita a retirada do produto pelo entregador, e as retiradas só podem ser feitas entre as 08:00 e 18:00h.
+
+        const { recipient_id, deliveryman_id, product, start_date } = req.body;
+
+        const response = await Order.create({
+            recipient_id,
+            deliveryman_id,
+            product,
+            start_date
+        });
+
+        return res.json({ response })
 
     }
 }
--- api/src/controllers/RecipientsController.js
+++ api/src/controllers/RecipientsController.js
@@ -3,11 +3,11 @@ import * as Yup from 'yup';
 import Recipient from '../models/Recipients';
 
 class RecipientController {
-    async store(req,res){
+    async store(req, res) {
 
         const schema = Yup.object({
             name: Yup.string().required().min(3),
-            street: Yup.string().required() ,
+            street: Yup.string().required(),
             number: Yup.number().required(),
             complement: Yup.string().min(3),
             state: Yup.string().required().max(2),
@@ -15,25 +15,24 @@ class RecipientController {
             zipCode: Yup.string().required().min(4),
         })
 
-        if(!(await schema.isValid(req.body))){
-            return res.status(401).json({error: 'Schema is not valid'});
+        if (!(await schema.isValid(req.body))) {
+            return res.status(401).json({ error: 'Schema is not valid' });
         }
 
-        const { name, street, number, complement, state, city, zipCode } = req.body;
+        const { name, street, number, complement, state, city, zipCode: zip_code } = req.body;
 
-        console.log(req.body);
 
         const response = await Recipient.create({
-            name, 
-            street, 
-            number, 
-            complement, 
-            state, 
-            city, 
-            zip_code: zipCode
+            name,
+            street,
+            number,
+            complement,
+            state,
+            city,
+            zip_code
         })
 
-        return res.json({response})
+        return res.json({ response })
 
     }
 }
