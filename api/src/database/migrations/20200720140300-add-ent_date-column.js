1c16cd4380effd41978d827befba8b685bab61c2
Cristian Silva <cristian123105@gmail.com>
2020-07-20 11:55:08 -0300

    chore: add migration to end_Date column to order

---
+++ api/src/database/migrations/20200720140300-add-ent_date-column.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('orders', 'end_date', {
+      type: Sequelize.DATE,
+      allowNull: true,
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('orders', 'end_date');
+
+  }
+};
